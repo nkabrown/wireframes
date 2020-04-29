20f74e470b6fc26a4a43f11056a61d0a2675750a
Nathan <nathan.brown@annalect.com>
2020-04-28 22:49:21 -0400

    Basic Drawer functionality

--- columns-and-drawer/js/index.js
+++ columns-and-drawer/js/index.js
@@ -1,1 +1,17 @@
-console.log('smoketest');
+// querySelectorAll returns a nodeList with can be iterated over
+// with array methods
+const placards = document.querySelectorAll('.placard');
+
+placards.forEach(placard => {
+  placard.addEventListener('click', () => {
+    const container = document.getElementsByClassName('content')[0];
+    const drawer = document.getElementsByClassName('image-drawer')[0];
+    if (drawer.getAttribute('mode') == 'open') {
+      drawer.setAttribute('mode', 'closed');
+      container.style.minWidth = '100%';
+    } else {
+      drawer.setAttribute('mode', 'open');
+      container.style.minWidth = '82%';
+    }
+  });
+});
